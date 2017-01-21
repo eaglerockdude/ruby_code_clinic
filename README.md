411bdefbb555a442d832301608e2522c513133b0
ken mcfadden <kendmcfadden@gmail.com>
2017-01-20 22:14:23 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ruby_code_clinic
+## Keep the brain fresh doing some raw ruby each day.
+
---
+++ lake_readings.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+require_relative('functions')
+puts "\n*** LAKE PEND OREILLE READINGS"
+puts "Calculates the mean and median of the wind speed,"
+puts "air temperature, and barometric pressure recorded at Deep Moore Station"
+puts "Calculates the mean and median of the wind speed,"
+
